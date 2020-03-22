0bf552eb3959abe4e2723585e0e83a7f81d5a311
Samuel Bassett <samabassett@gmail.com>
2020-03-22 16:58:16 +0100

    Updates

---
+++ precache-manifest.278ef0ec5a298232481719522f952d26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a53267f7ae3fce6dee6ab6cc89c24547",
+    "url": "/reactburger/index.html"
+  },
+  {
+    "revision": "2343fd3cbe618f3cba2a",
+    "url": "/reactburger/static/css/main.7f38baeb.chunk.css"
+  },
+  {
+    "revision": "88470b6dec5996bfa2fb",
+    "url": "/reactburger/static/js/2.52bb423e.chunk.js"
+  },
+  {
+    "revision": "e468b20662ce7acec401182a9064b329",
+    "url": "/reactburger/static/js/2.52bb423e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2343fd3cbe618f3cba2a",
+    "url": "/reactburger/static/js/main.d4369ef6.chunk.js"
+  },
+  {
+    "revision": "ad289fd43e42473626bc",
+    "url": "/reactburger/static/js/runtime-main.be625298.js"
+  }
+]);
